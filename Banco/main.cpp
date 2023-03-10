7848ce8a7295761b38a2618de1634470c6e16de6
GabrielMeloBatista <gabrielmelobatisa@hotmail.com>
2023-03-10 12:46:33 -0300

    Uso de namespaces colocado na main

--- Banco/main.cpp
+++ Banco/main.cpp
@@ -1,32 +1,54 @@
 #include <iostream>
 #include "Conta.hpp"
-#include "Titular.h"
+#include "ContaPoupanca.hpp"
+#include "ContaCorrente.hpp"
+#include "Titular.hpp"
 #include "CPF.hpp"
+#include "Funcionario.hpp"
+#include "Caixa.hpp"
+#include "Gerente.hpp"
 
 using namespace std;
 
-void ExibeSaldo(const Conta& conta)
+void ExibeSaldo(const Conta::Conta& conta)
 {
 	cout << "O Saldo da conta de " << conta.recuperaNomeTitular();
 	cout << " é de R$" << conta.recuperaSaldo() << endl;
 }
 
+void fazLogin(const Autenticacao::Autenticavel& alguem, const string senha)
+{
+	if (alguem.verificaSenha(senha))
+	{
+		cout << "Autenticado com sucesso" << endl;
+	}
+	else
+	{
+		cout << "Senha Invalida!!" << endl;
+	}
+}
+
 int main()
 {
-	Conta umaConta("13", Titular(CPF("123.456.789-10"), "Viniccius13"));
-	Conta umaOutraConta("14", Titular(CPF("123.456.789-14"), "Viniccius14"));
+	Titular::Titular vinnicius13(CPF::CPF("123.456.789-10"), "Viniccius13", "umaSenha");
+	Conta::ContaPoupanca umaConta("13", vinnicius13);
+	Conta::ContaCorrente umaOutraConta("14", Titular::Titular(CPF::CPF("123.456.789-14"), "Viniccius14", "outraSenha"));
+	Conta::ContaCorrente umaOutraContaCorrente("546312", vinnicius13);
 
 	umaConta.depositar(500.00);
 	umaConta.sacar(200.00);
 
 	ExibeSaldo(umaConta);
 
-	umaOutraConta.depositar(500.00);
-	umaOutraConta.sacar(300.00);
+	umaOutraConta.depositar(300.00);
+	umaOutraConta.transferePara(umaConta, 250);
+
+	umaOutraConta.transferePara(umaOutraContaCorrente, 250);
 
 	ExibeSaldo(umaOutraConta);
+	ExibeSaldo(umaOutraContaCorrente);
 
-	cout << "Numero de Contas: " << Conta::recuperaNumeroDeContas() << endl;
+	cout << "Numero de Contas: " << Conta::Conta::recuperaNumeroDeContas() << endl;
 
 	return 0;
 }
